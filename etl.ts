0bc5d3e59cc5242957a63e4f27678f957aae5bc2
EricGoyenecheaEsquina <102578352+EricGoyenecheaEsquina@users.noreply.github.com>
2022-03-29 08:57:46 +0200

    etl hecho

--- etl.ts
+++ etl.ts
@@ -1,3 +1,16 @@
-export function transform(/* Parameters go here */) {
-  throw new Error('Remove this statement and implement this function')
+export function transform(old: { [key: string]: string[] }) {
+  let array: { [key: string]: number } = {};
+  for (let key in old) {
+    console.log(key);
+    let letras: string[] = old[key];
+    let numero = parseInt(key);
+    letras.sort();
+    console.log(letras);
+    for (let entry of letras) {
+      let letra: string = entry.toLocaleLowerCase()
+      array[letra] = numero;
+    }
+
+  }
+  return array;
 }
